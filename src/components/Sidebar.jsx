e7b1a5f6d2b116fd7525c4502cb04b661880bb88
roberto <robpalacios1@hotmail.com>
2020-12-15 16:44:05 -0500

    add Sidebar.jsx component

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Sidebar.css'
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+            <h2>I am a Sidebar</h2>
+            
+            <div className="sidebar__top">
+                <h3>Roberto Palacios</h3>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
