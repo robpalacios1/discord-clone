2642610e0170f0cef2514fd302e4cf4212792c06
roberto <robpalacios1@hotmail.com>
2020-12-15 16:51:38 -0500

    import Sidebar.jsx component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Discord Clone!!!</h1>
+      {/** Sidebar */}
+      <Sidebar />
+
+      {/** Chat */}
     </div>
   );
 }
