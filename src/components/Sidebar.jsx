59edb9f35dc1afe338ae916fe69e681d89790b14
roberto <robpalacios1@hotmail.com>
2020-12-15 21:25:06 -0500

    add classes to left sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import AddIcon from '@material-ui/icons/Add';
 import './Sidebar.css'
 
 const Sidebar = () => {
     return (
         <div className="sidebar">
             <h2>I am a Sidebar</h2>
-            
             <div className="sidebar__top">
                 <h3>Roberto Palacios</h3>
+                <ExpandMoreIcon />
+            </div>
+            <div className="sidebar__channel">
+                <div className="sidebar__channelsHeader">
+                    <div className="sidebar__header">
+                        <ExpandMoreIcon />
+                        <h4>Text Channels</h4>
+                    </div>
+                    <AddIcon className="sidebar__addChannel"/>
+                </div>
             </div>
         </div>
     )
