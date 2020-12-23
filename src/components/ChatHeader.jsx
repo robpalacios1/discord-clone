a020dfbf24dd063730dea41fcdeb57474ba7910b
roberto <robpalacios1@hotmail.com>
2020-12-23 14:40:52 -0500

    add ChatHeader.jsx component

---
+++ src/components/ChatHeader.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+/*****ICONS*****/
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import EditLocationRoundedIcon from '@material-ui/icons/EditLocationRounded';
+import PeopleAltRoundedIcon from '@material-ui/icons/PeopleAltRounded';
+import SearchRoundedIcon  from '@material-ui/icons/Search';
+import HelpRoundedIcon  from '@material-ui/icons/Help';
+import SendRoundedIcon from '@material-ui/icons/SendRounded'
+
+/*****CSS*****/
+import './ChatHeader.css'
+
+const Chatheader = () => {
+    return (
+        <div className="chatHeader">
+            <div className="chatHeader__left">
+                <h3>
+                    <span className="chatHeader__hash">#</span>
+                    Test Channel Name
+                </h3>
+            </div>
+            <div className="chatHeader__right">
+                <NotificationsIcon />
+                <EditLocationRoundedIcon />
+                <PeopleAltRoundedIcon />
+
+                <div className="chatHeader__search">
+                    <input placeholder="Search" />
+                        <SearchRoundedIcon />
+                </div>
+
+                <SendRoundedIcon />
+                <HelpRoundedIcon />
+            </div>
+        </div>
+    )
+}
+
+export default Chatheader
