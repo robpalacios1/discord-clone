4f068657fc59caa92c5e178fe794d70ace9a55da
roberto <robpalacios1@hotmail.com>
2020-12-28 16:29:34 -0500

    change the state by state.user.user

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { useSelector } from 'react-redux';
+import { selectUser } from './features/userSlice'
 
 /** Component */
 import Sidebar from './components/Sidebar';
 import Chat from './components/Chat'
 
+
 /***** CSS *****/
 import './App.css';
 
 
+
 function App() {
+
+  const user = useSelector(selectUser);
+
   return (
     <div className="app">
-      <Sidebar />
-
-      <Chat />
+      {user ? (
+        <Fragment>
+          <Sidebar />
+          <Chat />
+        </Fragment>
+      ):(
+        <h2>You need to login</h2>
+      )}
     </div>
   );
 }
