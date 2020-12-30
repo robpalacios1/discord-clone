69f2c0e1110a68c668d93fb38cc29247b020cc88
roberto <robpalacios1@hotmail.com>
2020-12-30 11:37:34 -0500

    add event onClick

--- src/components/SidebarChannel.jsx
+++ src/components/SidebarChannel.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { setChannelInfo } from '../features/appSlice';
 
 /***** CSS *****/
 import './SidebarChannel.css'
 
 const Sidebarchannel = ({ id, channelName }) => {
+
+    const dispatch = useDispatch();
+
     return (
-        <div className="sidebarChannel">
+        <div className="sidebarChannel"
+            onClick={() =>
+                dispatch(
+                    setChannelInfo({
+                        channelId: id,
+                        channelName: channelName,
+                    })
+                )
+            }
+        >
             <h4>
                 <span className="sidebarChannel__hash">#</span>
                 {channelName}
