e66787e74c56852fcc07795d7f4fd995e19771f6
roberto <robpalacios1@hotmail.com>
2020-12-30 11:38:47 -0500

    add reducers

--- src/features/appSlice.js
+++ src/features/appSlice.js
@@ -1,20 +1,20 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 export const appSlice = createSlice({
-  name: 'counter',
+  name: 'app',
   initialState: {
-    app: 0,
     channelId: null,
     channelName: null
   },
   reducers: {
-    setChannelId: (state, action) => {
-      state.app = state.app + action.payload;
+    setChannelInfo: (state, action) => {
+      state.ChannelId = action.payload.channelId;
+      state.ChannelName = action.payload.channelName;
     },
   },
 });
 
-export const { setChannelId } = appSlice.actions;
+export const { setChannelInfo } = appSlice.actions;
 
 export const selectChannelId = (state) => state.app.channelId;
 export const selectChannelName = (state) => state.app.channelName;
