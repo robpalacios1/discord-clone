890497a482a075740599ce9396888e2e50bf0727
roberto <robpalacios1@hotmail.com>
2020-12-30 11:31:37 -0500

    add channelName props

--- src/components/ChatHeader.jsx
+++ src/components/ChatHeader.jsx
@@ -11,13 +11,13 @@ import SendRoundedIcon from '@material-ui/icons/SendRounded'
 /*****CSS*****/
 import './ChatHeader.css'
 
-const Chatheader = () => {
+const Chatheader = ({ channelName }) => {
     return (
         <div className="chatHeader">
             <div className="chatHeader__left">
                 <h3>
                     <span className="chatHeader__hash">#</span>
-                    Test Channel Name
+                    {channelName}
                 </h3>
             </div>
             <div className="chatHeader__right">
