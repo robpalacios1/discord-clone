b93eec97974fb3896037ae1dfce4fcf30b15a9cf
roberto <robpalacios1@hotmail.com>
2020-12-30 13:33:05 -0500

    change message parameter from messages

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -38,27 +38,33 @@ const Chat = () => {
                 setMessages(snapshot.docs.map((doc) => doc.data()))
             );
         }
-    }, [])
+    }, [channelId])
 
     const sendMessage = (e) => {
         e.preventDefault();
 
         db.collection('channels')
         .doc(channelId)
-        .collection('message')
+        .collection('messages')
         .add({
             timestamp: firebase.firestore.FieldValue.serverTimestamp(),
             message: input,
             user: user
         })
+        setInput('')
     }
 
     return (
         <div className="chat">
             <ChatHeader channelName={channelName}/>
+
             <div className="chat__messages">
-                {messages.map((Message) => (
-                    <Message />
+                {messages.map((message) => (
+                    <Message
+                        timestamp={message.timestamp}
+                        message={message.message}
+                        user={message.user}
+                    />
                 ))}
             </div>
 
@@ -72,8 +78,8 @@ const Chat = () => {
                         placeholder={`Message #${channelName}`}
                     />
                     <button
-                        className="chat__inputButton"
                         disabled={!channelId}
+                        className="chat__inputButton"
                         type="submit"
                         onClick={sendMessage}
                     >
