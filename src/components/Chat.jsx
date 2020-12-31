7fc005992e95ab084121ea054db052bfcae011b9
roberto <robpalacios1@hotmail.com>
2020-12-31 01:30:41 -0500

    import firebase from firebase

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react'
-import firebase from 'firebase';
+import firebase from 'firebase/app';
 import { useSelector } from 'react-redux';
 import { selectChannelId, selectChannelName } from '../features/appSlice';
 import { selectUser } from '../features/userSlice'
