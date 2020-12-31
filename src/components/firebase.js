38933e3ee5cea8e8f7f72233dedf613cd7b693a6
roberto <robpalacios1@hotmail.com>
2020-12-31 01:37:52 -0500

    fix const name

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -10,7 +10,7 @@ const firebaseConfig = {
   };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+const db = firebaseApp.firestore();
 const auth = firebase.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
 
